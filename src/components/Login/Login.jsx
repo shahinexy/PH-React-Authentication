81715143dbf6bf888a2728539f568323d3611aa1
shahinexy <shahinexy@gmail.com>
2024-03-30 07:33:27 +0600

    open google login popup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,20 @@
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 
 const Login = () => {
     const auth = getAuth(app);
+    console.log(auth);
     const provider = new GoogleAuthProvider();
 
     const handleGooleSingin = () =>{
-        console.log('google authentication');
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log('user result', user);
+        })
+        .catch(error =>{
+            console.log('error', error.message);
+        })
     }
 
     return (
