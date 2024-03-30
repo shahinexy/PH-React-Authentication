080f478b3bfe0d113c3d77f829a4b00069c04f1c
shahinexy <shahinexy@gmail.com>
2024-03-30 08:29:16 +0600

    github login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,69 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
     const auth = getAuth(app);
-    console.log(auth);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const gihubProvider = new GithubAuthProvider()
+
+    const [userData, setUserData] = useState(null)
 
     const handleGooleSingin = () =>{
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
         .then(result => {
             const user = result.user;
-            console.log('user result', user);
+            console.log('user result', user)
+            setUserData(user)
         })
         .catch(error =>{
             console.log('error', error.message);
         })
     }
 
+    const handleGithubLogin =()=>{
+        signInWithPopup(auth, gihubProvider)
+        .then(result => {
+            const user = result.user;
+            console.log('user result', user)
+            setUserData(user)
+        })
+        .catch(error =>{
+            console.log('error', error.message);
+        })
+    }
+
+    const handleSingOut = () =>{
+        signOut(auth) 
+        .then(result =>{
+            console.log('sing out result',result);
+            setUserData(null)
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
+
     return (
         <div>
             <h2>Login here</h2>
-            <button onClick={handleGooleSingin}>Login</button>
+            {
+                userData ? <button onClick={handleSingOut}>Sing Out</button>
+                 : <div>
+                    <button onClick={handleGooleSingin}>Google Login</button> 
+                    <button onClick={handleGithubLogin}>Github login</button>
+                 </div>
+            }
+            
+            
+            <div>
+                {
+                    userData && <div>
+                        <h3>User Name: {userData?.displayName}</h3>
+                                <p>User Email: {userData?.email}</p>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
